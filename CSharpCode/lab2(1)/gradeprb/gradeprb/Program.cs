6e273ebb0445ca18a2f619a318e7af16a59b3098
Md Sakib Hossain <sakib.aiubedu.cse@gmail.com>
2021-02-08 09:46:14 +0600

    lab2(1) Task

---
+++ CSharpCode/lab2(1)/gradeprb/gradeprb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gradeprb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float phy = 88f;
+            float chem = 86f;
+            float bio = 78f;
+            float math = 91f;
+            float comp = 92f;
+            float result = (phy + chem + bio + math + comp) / 5;
+            Console.WriteLine("Grade " + result);
+            if (result >= 90) { Console.WriteLine("Grade A+"); }
+            else if (result >= 85 && result < 90) 
+            { Console.WriteLine("Grade A"); }
+            else if (result >= 80 && result < 85) 
+            { Console.WriteLine("Grade B+"); }
+            else if (result >= 75 && result < 80)
+            { Console.WriteLine("Grade B"); }
+            else if (result >= 50 && result < 75)
+            { Console.WriteLine("Grade C+"); }
+            else { Console.WriteLine("Grade F"); }
+        }
+    }
+}
---
+++ CSharpCode/lab2(1)/sumofevenodd/sumofevenodd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumofevenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int even = 0;
+            int odd = 0;
+            int n = 100;
+            for (i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even = even + i;
+                }
+                else
+                    odd = odd + i;
+            }
+
+            Console.WriteLine("Even numbers sum: " + even);
+            Console.WriteLine("Even numbers sum: " + odd);
+        }
+    }
+}
